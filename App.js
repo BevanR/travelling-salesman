2a8c18cc5d2cda3c78adc49e6e89360f09006b99
Bevan Rudge <bevan@geneious.com>
2019-06-05 22:59:41 +1200

    Get initial geolocation working.

--- App.js
+++ App.js
@@ -1,21 +1,67 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import {StyleSheet, TextInput, View} from 'react-native'
+import MapView from 'react-native-maps'
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      deviceLocation: {},
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const {latitude, longitude} = position.coords
+        return this.setState({
+          deviceLocation: {latitude, longitude},
+        })
+      },
+      (error) => alert(error.message),
+      {
+        // enableHighAccuracy: true,
+        // timeout: 20000,
+        maximumAge: 100000,
+      },
+    )
+  }
+
   render() {
+    const {latitude, longitude} = this.state.deviceLocation
+    const delta = 0.05
+    const region = {latitude, longitude, latitudeDelta: delta, longitudeDelta: delta}
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        {
+          latitude && <MapView style={styles.map} initialRegion={region}/>
+        }
+
+        <TextInput placeholder="🔍 Search here" style={styles.search}/>
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
   },
-});
+  map: {
+    flex: 1,
+    width: '100%',
+  },
+  search: {
+    position: 'absolute',
+    top: 35,
+    left: 10,
+    right: 10,
+    height: 50,
+    padding: 3,
+    borderWidth: 1,
+    borderColor: 'grey',
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    textAlign: 'center',
+  },
+})
